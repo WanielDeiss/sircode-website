3fc36ee8459c68b75cf1c905b5ac1b1a3f2ff2c3
Daniel Weiß <wanieldeiss@gmail.com>
2020-09-13 11:18:17 +0200

    project init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
 import { Component } from '@angular/core';
+import {
+  faEtsy,
+  faGithub,
+  faInstagram,
+  faLinkedin,
+  faTwitter,
+  faXing,
+  IconDefinition,
+} from '@fortawesome/free-brands-svg-icons';
+import { faDice } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'sircode-website';
+  meOn: MeOn[] = [
+    {
+      platform: 'Xing',
+      icon: faXing,
+      url: 'https://www.xing.com/profile/Daniel_Weiss48/cv',
+    },
+    {
+      platform: 'LinkedIn',
+      icon: faLinkedin,
+      url: 'https://www.linkedin.com/in/wanieldeiss/',
+    },
+    {
+      platform: 'GitHub',
+      icon: faGithub,
+      url: 'https://github.com/WanielDeiss',
+    },
+    {
+      platform: 'BoardGameGeek',
+      icon: faDice,
+      url: 'https://boardgamegeek.com/user/WanielDeiss',
+    },
+    {
+      platform: 'Instagram',
+      icon: faInstagram,
+      url: 'https://www.instagram.com/wanieldeiss/',
+    },
+    {
+      platform: 'Etsy',
+      icon: faEtsy,
+      url: 'https://www.etsy.com/de/shop/DanielDice',
+    },
+    {
+      platform: 'Twitter',
+      icon: faTwitter,
+      url: 'https://twitter.com/WanielDeiss',
+    },
+  ];
+}
+
+interface MeOn {
+  platform: string;
+  icon: IconDefinition;
+  url: string;
 }
